1246656e163691a045eefd881e57f48746f7c287
eyancheryl <eyancheryl@gmail.com>
2023-07-13 04:50:02 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ * C Program that prints a line of text using puts
+ *
+ * **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
