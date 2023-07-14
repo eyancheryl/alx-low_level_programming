5193dab5cbe92dee27e72d17f7bdb674835336c6
eyancheryl <eyancheryl@gmail.com>
2023-07-14 06:15:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible different combinations of 2 digit.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tensDigit;
+	int onesDigit;
+
+	for (tensDigit = 0; tensDigit <= 8; tensDigit++)
+	{
+		for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
+		{
+			putchar(tensDigit + '0');
+			putchar(onesDigit + '0');
+
+			if (tensDigit != 8 || onesDigit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
