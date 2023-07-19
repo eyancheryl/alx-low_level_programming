02059c69e3c4654b896acd934350db2f1b456af3
eyancheryl <eyancheryl@gmail.com>
2023-07-19 00:40:16 -0700

    101-natural.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,27 +2,26 @@
 
 /**
  * print_times_table - Prints the n times table, starting with 0.
- * @n: The number of times table to be printed.
+ * @n: The number of rows and columns in the times table.
  */
-
+ii
 void print_times_table(int n)
 {
+if (n < 0 || n > 15)
+return;
+
 int i, j, result;
 
-if (n < 0 || n > 15))
-return;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
-
 if (j == 0)
 printf("%d", result);
 else
-printf(",%3d", result);
+printf(", %3d", result); // Format the output to align numbers properly
 }
-
 printf("\n");
 }
 }
