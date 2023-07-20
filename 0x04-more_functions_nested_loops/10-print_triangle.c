8ef2d0a552b9d6cd3441b7275133737993fd04f4
eyancheryl <eyancheryl@gmail.com>
2023-07-20 06:02:15 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-angled triangle of '#' characters
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
+}
+
+for (j = 0; j < i + 1; j++)
+{
+_putchar('#');
+}
+
+_putchar('\n');
+}
+}
+}
